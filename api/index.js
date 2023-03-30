e6c100245a79493da43298b671624f139d737a49
Ben Hur <Bhur777@gmail.com>
2023-03-30 17:23:17 -0400

    finish register and user cookie redirect

--- api/index.js
+++ api/index.js
@@ -1,9 +1,54 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const User = require("./models/User");
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		credentials: true,
+		origin: process.env.CLIENT_URL,
+	})
+);
 
-app.get("/test", (req, res) => {
-	res.json("TEST OK");
+mongoose.connect(process.env.MONGODB_URL);
+jwtSecret = process.env.JWT_SECRET;
+
+app.get("/profile", (req, res) => {
+	const { token } = req.cookies;
+	if (token) {
+		jwt.verify(token, jwtSecret, {}, (err, userData) => {
+			if (err) throw err;
+			res.json(userData);
+		});
+	} else {
+		res.status(420).json("No Token");
+	}
+});
+
+app.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	try {
+		const createdUser = await User.create({
+			username,
+			password,
+		});
+		jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
+			if (err) throw err;
+			res.cookie("token", token, { sameSite: "none", secure: true }).status(201).json({
+				id: createdUser._id,
+			});
+		});
+	} catch (err) {
+		if (err) throw err;
+		res.status(500).json("error");
+	}
 });
 
 app.listen(3000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import axios from "axios";
+import Routes from "./components/Routes";
+import { UserContextProvider } from "./components/UserContext";
 
 function App() {
-	return <div>Hello World</div>;
+	axios.defaults.baseURL = "http://localhost:3000";
+	axios.defaults.withCredentials = true;
+
+	return (
+		<div>
+			<UserContextProvider>
+				<Routes />
+			</UserContextProvider>
+		</div>
+	);
 }
 
 export default App;
